a39f030db83a815f06ece5dbc18cd388fdfaadc5
Ferdinand Bada <ferdinandkamata@duck.com>
2024-11-14 14:38:02 +0300

    cleanup

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -17,7 +17,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include, re_path
 from . import views
-from django.conf.urls.static import static
 from django.conf import settings
 from django.views.static import serve
 
@@ -30,5 +29,3 @@ urlpatterns = [
     path('posts/', include('posts.urls')),
     path('users/', include('users.urls')),
 ]
-
-# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
